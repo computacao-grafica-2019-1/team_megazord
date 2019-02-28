8aee90a93a91011a8f2f84f997934009ca90256b
rodrigovf <rodrigo.v.figueiredo@live.com>
2019-02-27 21:56:23 -0300

    Cabeça criada

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
   background(220);
+  smooth();
+
+
+  push();
+  translate(width/2,80)
+  rect(-30,-60, 60, 60)
+  pop();
+ 
+
+
 }
